8b4d72ef18e70a40350969b84a0a288f557042af
Olivier Chirouze <o.chirouze@criteo.com>
2020-01-31 17:30:11 +0100

    Reformat from prettier

--- src/competitions/2020-training/src/generators/dumb-generator.ts
+++ src/competitions/2020-training/src/generators/dumb-generator.ts
@@ -1,29 +1,26 @@
-import { ISolutionGenerator } from "../../../../hashcode-tooling/i-solution-generator";
-import { Input } from "../models/input";
-import { Output } from "../models/output";
+import { ISolutionGenerator } from '../../../../hashcode-tooling/i-solution-generator';
+import { Input } from '../models/input';
+import { Output } from '../models/output';
 
 export class DumbGenerator implements ISolutionGenerator<Input, Output> {
+  hasNextGenerator: boolean = true;
+  getName(): string {
+    return 'Dumb';
+  }
 
-    hasNextGenerator: boolean = true;
-    getName(): string {
-        return "Dumb";
-    }    
-    
-    next(preConditions: Input): Output {
-        const solution: Output = new Output();
-        let i = 0;
-        while (solution.getScore(preConditions) + preConditions.pizzaTypes[i] <= preConditions.maximumSlices) {
-            solution.orderedPizzaTypes.push(i);
-            i++;
-        }
-
-        this.hasNextGenerator = false;
-        return solution;
+  next(preConditions: Input): Output {
+    const solution: Output = new Output();
+    let i = 0;
+    while (solution.getScore(preConditions) + preConditions.pizzaTypes[i] <= preConditions.maximumSlices) {
+      solution.orderedPizzaTypes.push(i);
+      i++;
     }
 
-    hasNext():boolean {
-        return this.hasNextGenerator;
-    }
+    this.hasNextGenerator = false;
+    return solution;
+  }
 
-    
+  hasNext(): boolean {
+    return this.hasNextGenerator;
+  }
 }
--- src/competitions/2020-training/src/models/input.ts
+++ src/competitions/2020-training/src/models/input.ts
@@ -1,15 +1,14 @@
-import { Scanner } from "../../../../hashcode-tooling/files/scanner";
+import { Scanner } from '../../../../hashcode-tooling/files/scanner';
 
 export class Input {
+  pizzaTypes: Array<number> = [];
+  maximumSlices: number;
 
-    pizzaTypes:Array<number> = [];
-    maximumSlices:number;
-
-    constructor(scanner:Scanner) {
-        this.maximumSlices = scanner.nextNumber();
-        const nbPizzaTypes:number = scanner.nextNumber();
-        for (let i=0; i< nbPizzaTypes; i++) {
-            this.pizzaTypes.push(scanner.nextNumber());
-        }
+  constructor(scanner: Scanner) {
+    this.maximumSlices = scanner.nextNumber();
+    const nbPizzaTypes: number = scanner.nextNumber();
+    for (let i = 0; i < nbPizzaTypes; i++) {
+      this.pizzaTypes.push(scanner.nextNumber());
     }
+  }
 }
