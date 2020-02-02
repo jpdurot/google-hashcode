418cd6d5c057db9a429b5f08fa3b9a64a46413c3
Olivier Chirouze <o.chirouze@criteo.com>
2020-02-02 19:52:01 +0100

    Replace Dictionary with Set

--- src/competitions/2020-training/src/generators/random-generator.ts
+++ src/competitions/2020-training/src/generators/random-generator.ts
@@ -18,13 +18,13 @@ export class RandomGenerator implements ISolutionGenerator<Input, Output> {
 
     const solution = new Output(preConditions);
 
-    const availablePizzaTypes = new Collections.Dictionary<number, boolean>();
-    preConditions.pizzaTypes.forEach((type, i) => availablePizzaTypes.setValue(i, true));
+    const availablePizzaTypes = new Collections.Set<number>();
+    preConditions.pizzaTypes.forEach((type, i) => availablePizzaTypes.add(i));
 
     // console.log(`Available types: ${availablePizzaTypes.size()}`);
 
     while (availablePizzaTypes.size() != 0) {
-      const i = randomInArray(availablePizzaTypes.keys());
+      const i = randomInArray(availablePizzaTypes.toArray());
 
       //console.log(availablePizzaTypes.keys());
 
@@ -34,6 +34,7 @@ export class RandomGenerator implements ISolutionGenerator<Input, Output> {
         solution.rollback();
         break;
       } else {
+        // TODO this could be done inside the Output object
         availablePizzaTypes.remove(i);
       }
     }
