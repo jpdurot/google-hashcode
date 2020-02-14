5dd76bc0e064809c72ab3d88bdf3e2b09bc646fe
Olivier Chirouze <o.chirouze@criteo.com>
2020-02-14 13:46:06 +0100

    Introduce index-dictionary

---
+++ src/hashcode-tooling/utils/index-dictionary.ts
@@ -0,0 +1,24 @@
+import { Dictionary } from 'typescript-collections';
+
+export class IndexDictionary<T> {
+  _dictionary = new Dictionary<T, number>();
+  _lastIndex = -1;
+
+  get(t: T): number | undefined {
+    return this._dictionary.getValue(t);
+  }
+
+  unset(t: T): void {
+    this._dictionary.remove(t);
+  }
+
+  add(t: T): number {
+    let index = this._lastIndex++;
+    this._dictionary.setValue(t, index);
+    return index;
+  }
+
+  getOrAdd(t: T): number {
+    return this.get(t) || this.add(t);
+  }
+}
