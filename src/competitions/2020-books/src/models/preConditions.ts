32ede01ee936a29aac05aa3c2727340dafcdafd1
Jean-Philippe Durot <jeanphilippe.durot@gmail.com>
2020-02-20 18:05:07 +0000

    preconditions

--- src/competitions/2020-books/src/models/preConditions.ts
+++ src/competitions/2020-books/src/models/preConditions.ts
@@ -1,7 +1,31 @@
 import { Scanner } from '../../../../hashcode-tooling/files/scanner';
 
 export class PreConditions {
+  public numberOfBooks: number;
+  public numberOfDays: number;
+  public numberOfLibraries: number;
+
+  public books:Array<Book> = [];
+  public booksById: { [id:number] : Book} = {}
+
   constructor(scanner: Scanner) {
-    //this.nbVideos = scanner.nextInt();
+    this.numberOfBooks = scanner.nextInt();
+    this.numberOfLibraries = scanner.nextInt();
+    this.numberOfDays = scanner.nextInt();
+
+    for (let b=0;b< this.numberOfBooks; b++)
+    {
+      const book = new Book(b, scanner.nextInt();
+      this.books.push(book);
+      this.booksById[book.id] = book;
+    }
+
+  }
+}
+
+export class Book {
+
+  constructor(public id:number, public score:number) {
+
   }
 }
