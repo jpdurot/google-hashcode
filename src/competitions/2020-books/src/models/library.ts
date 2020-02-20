92984a7a9df7a5eeedfb8195f85a15172d347462
Olivier Chirouze <o.chirouze@criteo.com>
2020-02-20 20:29:00 +0100

    Add library score

--- src/competitions/2020-books/src/models/library.ts
+++ src/competitions/2020-books/src/models/library.ts
@@ -1,5 +1,15 @@
+import { Book } from './book';
+import math = require('mathjs');
+
 export class Library {
   public bookIds: number[] = [];
+  public score?: number;
 
   constructor(public id: number, public signupDays: number, public booksPerDay: number) {}
+
+  public calculateScore(books: Book[], numberOfDays: number) {
+    // score = average of score of all books * booksPerDay * (this.numberOfDays - signUpDays)
+    this.score =
+      math.mean(...this.bookIds.map(id => books[id].score)) * this.booksPerDay * (numberOfDays - this.signupDays);
+  }
 }
