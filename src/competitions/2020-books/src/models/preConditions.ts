c07898e77a8a792e73729a1ceabc8e45d4e05dfd
Olivier Chirouze <o.chirouze@criteo.com>
2020-02-20 19:07:06 +0100

    Fix book

--- src/competitions/2020-books/src/models/preConditions.ts
+++ src/competitions/2020-books/src/models/preConditions.ts
@@ -1,31 +1,23 @@
 import { Scanner } from '../../../../hashcode-tooling/files/scanner';
+import { Book } from './book';
 
 export class PreConditions {
   public numberOfBooks: number;
   public numberOfDays: number;
   public numberOfLibraries: number;
 
-  public books:Array<Book> = [];
-  public booksById: { [id:number] : Book} = {}
+  public books: Array<Book> = [];
+  public booksById: { [id: number]: Book } = {};
 
   constructor(scanner: Scanner) {
     this.numberOfBooks = scanner.nextInt();
     this.numberOfLibraries = scanner.nextInt();
     this.numberOfDays = scanner.nextInt();
 
-    for (let b=0;b< this.numberOfBooks; b++)
-    {
-      const book = new Book(b, scanner.nextInt();
+    for (let b = 0; b < this.numberOfBooks; b++) {
+      const book = new Book(b, scanner.nextInt());
       this.books.push(book);
       this.booksById[book.id] = book;
     }
-
-  }
-}
-
-export class Book {
-
-  constructor(public id:number, public score:number) {
-
   }
 }
