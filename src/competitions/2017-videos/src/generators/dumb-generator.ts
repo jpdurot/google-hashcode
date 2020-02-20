08133e13529793037d7b8fee3460ed61dfeb120d
Olivier Chirouze <o.chirouze@criteo.com>
2020-02-20 16:15:15 +0100

    Videos: first dumb generator!!

--- src/competitions/2017-videos/src/generators/dumb-generator.ts
+++ src/competitions/2017-videos/src/generators/dumb-generator.ts
@@ -1,7 +1,7 @@
 import { ISolutionGenerator } from '../../../../hashcode-tooling/i-solution-generator';
 import { VideoState } from '../models/videoState';
 import { CacheTopology } from '../models/cacheTopology';
-import { Logger } from '../../../../hashcode-tooling/utils/logger';
+import { sum } from '../../../../hashcode-tooling/utils/array-util';
 
 export class DumbGenerator implements ISolutionGenerator<VideoState, CacheTopology> {
   static NAME = 'Dumb';
@@ -15,9 +15,29 @@ export class DumbGenerator implements ISolutionGenerator<VideoState, CacheTopolo
     // This is one shot
     this.hasNextGenerator = false;
 
-    //Logger.log(preConditions);
+    let cacheTopology = new CacheTopology(preConditions);
 
-    return new CacheTopology(preConditions);
+    // Take all videos, put them in the first cache available and stop
+    for (const video of preConditions.videos) {
+      for (let cacheId = 0; cacheId < preConditions.nbCaches; cacheId++) {
+        // In any case, initialize
+        cacheTopology.videosByCache[cacheId] = cacheTopology.videosByCache[cacheId] ?? {
+          cacheId,
+          videos: []
+        };
+
+        const spaceAvailable =
+          preConditions.cacheSize - cacheTopology.videosByCache[cacheId].videos.map(v => v.size).reduce(sum, 0);
+
+        if (spaceAvailable >= video.size) {
+          cacheTopology.videosByCache[cacheId].videos.push(video);
+          // ---------------- Put video only in one cache
+          break;
+        }
+      }
+    }
+
+    return cacheTopology;
   }
 
   hasNext(): boolean {
