d560fbb0f62569536c6894e0861d2830efb50524
Olivier Chirouze <o.chirouze@criteo.com>
2020-02-20 16:31:57 +0100

    Introduce bootstrap for new competition

---
+++ src/competitions/XXXX-bootstrap/src/index.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+import { SolutionFinder } from '../../../hashcode-tooling/solution-finder';
+import commandLineArgs = require('command-line-args');
+import { GeneratorFactory } from './generator-factory';
+import { readFilesFrom } from '../../../hashcode-tooling/utils/file-utils';
+import * as path from 'path';
+import { PreConditions } from './models/preConditions';
+
+// Dirty hack to get the real directory, since we know we run in /dist...
+const competitionName = path.dirname(__dirname).replace(path.dirname(path.dirname(__dirname)) + '/', '');
+const inputDataDir = path.join(
+  ...[
+    path.dirname(path.dirname(path.dirname(path.dirname(__dirname)))),
+    'src',
+    'competitions',
+    competitionName,
+    'input'
+  ]
+);
+const inputFiles = readFilesFrom(inputDataDir)
+  .map(f => f.name)
+  .filter(n => n !== '.gitkeep' && !n.endsWith('.pdf'))
+  .map(n => path.join(inputDataDir, n));
+
+const optionDefinitions = [{ name: 'generator', alias: 'g', multiple: true, type: String }];
+
+const options = commandLineArgs(optionDefinitions);
+
+options.generator.forEach((gName: string) =>
+  SolutionFinder.launchOnSeveralFiles(
+    inputFiles,
+    scanner => new PreConditions(scanner),
+    () => GeneratorFactory.from(gName)
+  )
+);
---
+++ src/competitions/XXXX-bootstrap/src/models/preConditions.ts
@@ -0,0 +1,7 @@
+import { Scanner } from '../../../../hashcode-tooling/files/scanner';
+
+export class PreConditions {
+  constructor(scanner: Scanner) {
+    //this.nbVideos = scanner.nextInt();
+  }
+}
